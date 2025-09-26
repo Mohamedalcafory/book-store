1ca6ec53ff17e8658f6921344360762b2231e5c6
Mohamedalcafory <mohamed.alcafory.456@gmail.com>
2025-09-26 17:21:22 +0300

    Add Category model and establish relationship with Book model

--- app/models/book.py
+++ app/models/book.py
@@ -17,6 +17,7 @@ class Book(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey("authors.id"), nullable=False)
     author = db.relationship("Author", backpopulates="books")
     category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+    category = db.relationship("Category", backpopulates="books")
     stock = db.Column(db.Integer, nullable=False)
     creator = db.Column(db.String(120), nullable=False)
 
---
+++ app/models/category.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from app import db
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False, index=True)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    )
+
+    books = db.relationship("Book", backpopulates="category", lazy=True)
+
+    def __repr__(self) -> str:
+        return f"<Category id={self.id} name={self.name!r}>"
+
+
